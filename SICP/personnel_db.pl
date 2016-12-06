5cd4f888ef12a04b521908d909d911c6fb4a1629
Piyo <piyo-ko@zj8.so-net.ne.jp>
2016-12-07 01:58:54 +0900

    :rocket: SICP §4.4.1のサンプルDBに対応するPrologファイルを作った。本文中でも言及した。

---
+++ SICP/personnel_db.pl
@@ -0,0 +1,59 @@
+% | ?- consult('personnel_db.pl').
+% で本ファイルを読み込む。なお、
+% | ?- listing.
+% で内容を確認できる。
+
+% GNU Prologでは、同じ述語を連続して (まとめて) 記述しておかないと
+% 「discontiguous predicate address/2 - clause ignored」
+% のようなエラーが出るので、それを回避するためのおまじない。
+:- discontiguous(address/2, job/2, salary/2, supervisor/2, can_do_job/2).
+
+address(['Bitdiddle', 'Ben'], ['Slumerville', ['Ridge', 'Road'], 10]).
+job(['Bitdiddle', 'Ben'], [computer, wizard]).
+salary(['Bitdiddle', 'Ben'], 60000).
+
+address(['Hacker', 'Alyssa', 'P'], ['Cambridge', ['Mass', 'Ave'], 78]).
+job(['Hacker', 'Alyssa', 'P'], [computer, programmer]).
+salary(['Hacker', 'Alyssa', 'P'], 40000).
+supervisor(['Hacker', 'Alyssa', 'P'], ['Bitdiddle', 'Ben']).
+
+address(['Fect', 'Cy', 'D'], ['Cambridge', ['Ames', 'Street'], 3]).
+job(['Fect', 'Cy', 'D'], [computer, programmer]).
+salary(['Fect', 'Cy', 'D'], 35000).
+supervisor(['Fect', 'Cy', 'D'], ['Bitdiddle', 'Ben']).
+
+address(['Tweakit', 'Lem', 'E'], ['Boston', ['Bay', 'State', 'Road'], 22]).
+job(['Tweakit', 'Lem', 'E'], [computer, technician]).
+salary(['Tweakit', 'Lem', 'E'], 25000).
+supervisor(['Tweakit', 'Lem', 'E'], ['Bitdiddle', 'Ben']).
+
+address(['Reasoner', 'Louis'], ['Slumerville', ['Pine', 'Tree', 'Road'], 80]).
+job(['Reasoner', 'Louis'], [computer, programmer, trainee]).
+salary(['Reasoner', 'Louis'], 30000).
+supervisor(['Reasoner', 'Louis'], ['Hacker', 'Alyssa', 'P']).
+
+supervisor(['Bitdiddle', 'Ben'], ['Warbucks', 'Oliver']).
+
+address(['Warbucks', 'Oliver'], ['Swellesley', ['Top', 'Heap', 'Road']]).
+job(['Warbucks', 'Oliver'], [administration, big, wheel]).
+salary(['Warbucks', 'Oliver'], 150000).
+
+address(['Scrooge', 'Eben'], ['Weston', ['Shady', 'Lane'], 10]).
+job(['Scrooge', 'Eben'], [accounting, chief, accountant]).
+salary(['Scrooge', 'Eben'], 75000).
+supervisor(['Scrooge', 'Eben'], ['Warbucks', 'Oliver']).
+
+address(['Cratchet', 'Robert'], ['Allston', ['N', 'Harvard', 'Street'], 16]).
+job(['Cratchet', 'Robert'], [accounting, scrivener]).
+salary(['Cratchet', 'Robert'], 18000).
+supervisor(['Cratchet', 'Robert'], ['Scrooge', 'Eben']).
+
+address(['Aull', 'DeWitt'], ['Slumerville', ['Onion', 'Square'], 5]).
+job(['Aull', 'DeWitt'], [administration, secretary]).
+salary(['Aull', 'DeWitt'], 25000).
+supervisor(['Aull', 'DeWitt'], ['Warbucks', 'Oliver']).
+
+can_do_job([computer, wizard], [computer, programmer]).
+can_do_job([computer, wizard], [computer, technician]).
+can_do_job([computer, programmer], [computer, programmer, trainee]).
+can_do_job([administration, secretary], [administration, big, wheel]).
